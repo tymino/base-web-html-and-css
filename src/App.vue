50dbc26c768adccd894b7afb47086fd5a0d5d1b6
Aleksey <d_team-lex@bk.ru>
2025-02-14 17:53:59 +0300

    add custom-button (no style)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CustomButton from './components/custom-button.vue'
+</script>
 
 <template>
   <div class="text-3xl text-red-600">Hello Vue</div>
+  <custom-button type="button" view="primary" color="black" name="Click me" />
 </template>
---
+++ src/components/custom-button.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface IProps {
+  name: string
+  view: 'primary' | 'secondary'
+  color: 'black' | 'green' | 'white'
+  type: 'button' | 'submit'
+}
+const props = defineProps<IProps>()
+
+const colorToTailwind = {
+  black: 'text-black',
+  green: 'text-green',
+  white: 'text-white',
+}
+
+const baseStyle = ''
+
+const typeStyle = computed(() => {
+  switch (props.view) {
+    case 'primary':
+      return `w-[160px] h-[50px] bg-primary-light text-secondary ${
+        colorToTailwind[props.color]
+      }`
+    case 'secondary':
+      return `w-[100px] h-[40px] border border-primary-light text-primary-light ${
+        colorToTailwind[props.color]
+      }`
+    default:
+      return ''
+  }
+})
+</script>
+
+<template>
+  <button :class="`${baseStyle} ${typeStyle}`" :type="type">
+    {{ name }}
+  </button>
+</template>
