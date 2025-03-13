c448523ed6bc4275fc7d3b7c09741c4f6d94bc87
tymino <d_team-lex@bk.ru>
2025-03-13 19:32:39 +0300

    s

---
+++ src/components/custom-button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="py-2 px-4 rounded-md text-white"
+    :style="{ borderColor: borderColor }"
+  >
+    {{ name }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string
+  borderColor: string
+}>()
+</script>
