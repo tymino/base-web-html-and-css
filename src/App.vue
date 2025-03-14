c3273d911a322fc99bb4919bd6ae89d9612be031
tymino <d_team-lex@bk.ru>
2025-03-14 20:07:38 +0300

    default colors twcss

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,14 @@ import CustomButton from './components/custom-button.vue'
 <template>
   <div class="text-3xl text-red-600">Hello Vue</div>
   <custom-button type="button" view="primary" color="black" name="Click me" />
+  <div class="bg-black">
+    <img
+      src="https://ucarecdn.com/a97e2ecc-3825-443f-8a94-6a912d1c4fe9/"
+      alt=""
+    />
+  </div>
+  <div class="flex flex-col justify-center items-center mt-5">
+    <h1>Title title</h1>
+    <p>Hello</p>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: '#000000',
+      secondary: '#00997D',
+      neutral: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
