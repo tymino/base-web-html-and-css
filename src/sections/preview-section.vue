dbea030b9d8c868c5ac6110cce7b35f609709572
tymino <d_team-lex@bk.ru>
2025-03-27 19:59:53 +0300

    preview block

--- src/sections/preview-section.vue
+++ src/sections/preview-section.vue
@@ -1,20 +1,51 @@
 <script setup lang="ts">
 import mainLogo from '../components/main-logo.vue'
 import navMenu from '../components/nav-menu.vue'
+import customButton from '../components/custom-button.vue'
 </script>
 
 <template>
   <div class="container">
-    <img
-      class="bg-img"
-      src="https://ucarecdn.com/761233c4-7224-418d-b151-ea7bc1196250/"
-      alt=""
-    />
+    <div class="bg-image-overlay">
+      <img
+        class="bg-image"
+        src="https://ucarecdn.com/761233c4-7224-418d-b151-ea7bc1196250/"
+        alt=""
+      />
+    </div>
 
     <div class="content">
       <div class="header">
         <main-logo />
         <nav-menu />
+      </div>
+      <div class="body">
+        <h1 class="body__title">
+          компания по разработке программного обеспечения
+        </h1>
+        <div class="body__button">
+          <custom-button
+            type="button"
+            view="secondary"
+            color="white"
+            name="запросить консультацию"
+          />
+        </div>
+        <div class="body__arrow">
+          <svg
+            width="42"
+            height="72"
+            viewBox="0 0 42 72"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M21 -18V71M21 71L1 44.6296M21 71L41 44.6296"
+              stroke="white"
+              stroke-width="2"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
@@ -26,7 +57,8 @@ import navMenu from '../components/nav-menu.vue'
   overflow: hidden;
 }
 
-.bg-img {
+.bg-image {
+  position: relative;
   display: block;
   width: 100%;
   height: auto;
@@ -34,6 +66,16 @@ import navMenu from '../components/nav-menu.vue'
   object-fit: cover;
   object-position: 0 200px;
   z-index: -10;
+
+  &-overlay::after {
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(var(--color-primary), 0.65);
+  }
 }
 
 .content {
@@ -42,6 +84,7 @@ import navMenu from '../components/nav-menu.vue'
   top: 0;
   left: 0;
   width: 100%;
+  height: 100%;
 }
 
 .header {
@@ -49,6 +92,34 @@ import navMenu from '../components/nav-menu.vue'
   justify-content: space-between;
   align-items: center;
   padding: 14px 6%;
-  background-color: rgba(var(--color-secondary), 0.12);
+  background: rgba(var(--color-secondary), 0.12);
+}
+
+.body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  &__title {
+    max-width: 820px;
+    margin-bottom: 60px;
+    color: var(--cl-neutral);
+    font-size: 47px;
+    font-weight: 200;
+    text-align: center;
+  }
+
+  &__button {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 48px;
+  }
+
+  &__arrow {
+    color: var(--cl-neutral);
+    text-align: center;
+  }
 }
 </style>
