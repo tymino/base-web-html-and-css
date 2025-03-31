b7f01a167a15541b7e646ca22a23d3d6ffc0940c
tymino <d_team-lex@bk.ru>
2025-03-31 18:17:47 +0300

    garant section

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
 import previewSection from './sections/preview-section.vue'
 import reviewSection from './sections/review-section.vue'
+import garantSection from './sections/garant-section.vue'
 </script>
 
 <template>
   <div class="content">
-    <div class="preview-section">
-      <preview-section />
-    </div>
-    <div class="review-section">
-      <review-section />
-    </div>
+    <div class="preview-section"><preview-section /></div>
+
+    <div class="review-section side-padding"><review-section /></div>
+
+    <div class="garant-section side-padding"><garant-section /></div>
 
     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.side-padding {
+  padding: 0 3%;
+}
 </style>
---
+++ src/sections/garant-section.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import customButton from '../components/custom-button.vue'
+</script>
+
+<template>
+  <div class="container">
+    <img
+      class="image"
+      src="https://ucarecdn.com/83398f66-832a-4a52-9930-0549e372be61/"
+      alt="tolpa"
+    />
+    <div class="description">
+      <div class="header">Гарантируем качество и выполнение в сроки</div>
+      <div class="body">
+        <p>
+          Мы просто умеем разрабатывать то, что нужно. То, что будет работать!
+        </p>
+        <div class="button">
+          <customButton
+            name="Запросить консультацию"
+            type="button"
+            view="primary"
+            color="black"
+          />
+        </div>
+      </div>
+
+      <div class="footer">
+        С 2010 года профессионально разрабатываем успешные мобильные и
+        веб-решения, индивидуальные програ ммные решения, информационные системы
+        и ПО для бизнеса, которые повышают эффективность бизнеса.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+.image {
+  width: auto;
+  max-height: 600px;
+}
+
+.description {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 60px 20px;
+
+  text-align: center;
+}
+
+.header {
+  position: relative;
+  max-width: 600px;
+  margin-bottom: 40px;
+  font-size: 40px;
+  font-weight: bold;
+
+  &::after {
+    content: '”';
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(24px, -24px);
+
+    font-family: 'Helvetica Neue', sans-serif;
+    color: lightgray;
+    font-size: 120px;
+  }
+
+  &::before {
+    content: '“';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    transform: translate(-20px, 60%);
+
+    font-family: 'Helvetica Neue', sans-serif;
+    color: lightgray;
+    font-size: 120px;
+  }
+}
+
+.body {
+  p {
+    max-width: 500px;
+
+    font-size: 20px;
+    font-weight: 400;
+    letter-spacing: 1px;
+  }
+
+  .button {
+    margin-top: 14px;
+  }
+}
+
+.footer {
+  max-width: 600px;
+  font-size: 13px;
+  font-weight: 300;
+  letter-spacing: 1px;
+}
+</style>
