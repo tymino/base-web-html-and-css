d18fd649df2afbbad7b24a224ff33e644c6d7c90
tymino <d_team-lex@bk.ru>
2025-03-31 16:49:46 +0300

    review section

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import previewSection from './sections/preview-section.vue'
+import reviewSection from './sections/review-section.vue'
 </script>
 
 <template>
   <div class="content">
-    <div class="main-section">
+    <div class="preview-section">
       <preview-section />
     </div>
+    <div class="review-section">
+      <review-section />
+    </div>
+
     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
---
+++ src/sections/review-section.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const reviewNames = [
+  { id: 0, header: '98%', desc: 'завершенных проектов успешно' },
+  { id: 1, header: '30', desc: 'календарных дней до первого релиза' },
+  { id: 2, header: '5+', desc: 'дней от идеи до готовой концепции' },
+  { id: 3, header: '+20%', desc: 'к эффективности бизнеса' },
+]
+</script>
+
+<template>
+  <div class="container">
+    <div class="review" v-for="{ id, header, desc } in reviewNames" :key="id">
+      <h2 class="title">{{ header }}</h2>
+      <p class="desc">{{ desc }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 12% auto;
+}
+
+.review {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 300px;
+  padding: 0 20px;
+  letter-spacing: 2px;
+  text-align: center;
+
+  &:not(:last-child) {
+    border-right: 2px solid var(--cl-secondary);
+  }
+}
+
+.title {
+  margin-bottom: 10px;
+  font-size: 48px;
+  font-weight: 600;
+}
+
+.desc {
+  font-size: 16px;
+  font-weight: 400;
+}
+</style>
