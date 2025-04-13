698a2d679741adda6a616e9fa8626647f29f8804
tymino <d_team-lex@bk.ru>
2025-04-13 16:57:56 +0300

    footer

---
+++ src/components/link-list.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface IProps {
+  names: string[]
+}
+
+defineProps<IProps>()
+</script>
+
+<template>
+  <div class="link-list">
+    <a class="link" v-for="name in names" :key="name" href="#">{{ name }}</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.link-list {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.link {
+  color: var(--cl-primary);
+  font-size: 18px;
+  font-weight: 300;
+  text-decoration: none;
+}
+</style>
--- src/sections/footer-section.vue
+++ src/sections/footer-section.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import mainLogo from '../components/main-logo.vue'
 import contactList from '../components/contact-list.vue'
+import linkList from '../components/link-list.vue'
 
 const phones = ['+7 (727) 334-21-45', '+7 (707) 998-93-90']
 const mails = ['info@devcloud.kz']
 const whatapp = ['Whatsapp']
 const telegram = ['telegram']
+const links1 = ['Главная', 'Портфолио', 'Услуги']
+const links2 = ['Процессы', 'О компании', 'Контакты']
 </script>
 
 <template>
@@ -27,20 +30,47 @@ const telegram = ['telegram']
         />
       </div>
     </div>
+
+    <div class="column-links-1">
+      <link-list :names="links1" />
+    </div>
+    <div class="column-links-2">
+      <link-list :names="links2" />
+    </div>
+
+    <div class="column-arrow">
+      <svg
+        width="34"
+        height="57"
+        viewBox="0 0 34 57"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M17 57.3999L17 1.08562M17 1.08562L33 17.7713M17 1.08562L1 17.7713"
+          stroke="black"
+        />
+      </svg>
+    </div>
+    <div class="copyright">
+      © Devcloud Solutions, Inc. 2021. We love our users!
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
+  position: relative;
   display: flex;
-
-  margin-top: 130px;
-  padding: 60px 0;
+  justify-content: space-between;
+  margin: 60px auto;
+  padding: 120px 0;
 }
 
 .column-logo {
   display: flex;
   flex-direction: column;
+  align-items: end;
   margin-bottom: 40px;
 }
 
@@ -52,5 +82,21 @@ const telegram = ['telegram']
   div:first-child {
     margin-right: 12px;
   }
+}
+
+// .column-arrow {
+// }
+
+.copyright {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 80px;
+  padding: 30px 20px 0 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.4);
+  font-size: 15px;
+  font-weight: 300;
+  text-align: end;
 }
 </style>
